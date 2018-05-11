fd2052b078e189ceded270440b413b4999d76cc4
faraday58 <jarodriguezvera@gmail.com>
2018-05-11 12:02:36 -0500

    Agregar archivos de proyecto.

---
+++ EscribirLeerArchivos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscribirLeerArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
